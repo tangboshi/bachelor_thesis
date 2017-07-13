8189f546fd944c62208fd8469563d75532713c58
tangboshi <info@alexander-pastor.de>
2017-07-13 10:53:13 +0200

    automatic code push

--- measurements/remote_measurement.sh
+++ measurements/remote_measurement.sh
@@ -1,8 +1,8 @@
 # Taking the measurement
 echo "Hello from remote_measurement.sh"
 cd 0_ba/bachelor_thesis
-git pull
-git fetch
+# git pull
+# git fetch
 cd measurements
 source measurement.conf
 export REMOTE_MEASUREMENT=0

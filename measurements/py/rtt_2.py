3b8219517c628c5dcda7074d7de5d27752cfd177
tangboshi <info@alexander-pastor.de>
2017-07-17 14:54:03 +0200

    automatic code push

--- measurements/py/rtt_2.py
+++ measurements/py/rtt_2.py
@@ -76,7 +76,9 @@ for i in range(1,repetitions+1):
 
     else:
         print(  "File "+retxs_path+" not found. \
-                Assuming not reached in GR.")
+                Assuming not reached in GR. Creating data for you...")
+        for index in range(ack_received_times):
+            retxs += [0]
 
 
     # Calculate RTT for each packet

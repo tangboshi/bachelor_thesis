9206bdaf9698a14a2068207aba2c92d7407d11ca
tangboshi <info@alexander-pastor.de>
2017-07-18 15:36:39 +0200

    automatic code push

--- measurements/py/throughput.py
+++ measurements/py/throughput.py
@@ -38,7 +38,10 @@ for i in range(1,repetitions+1):
         data[i-1] = 0
     if pt.isfile(file_path):
         datacount = lines.linecount(file_path)
-        data[i-1] = min(ackcount, datacount)*packet_size
+        if min(ackcount, datacount) == 0:
+            data[i-1] = 0
+        else:
+            data[i-1] = min(ackcount, datacount)*packet_size
     else:
         # no data sent off
         print("Data file not found. Probably zero throughput.")

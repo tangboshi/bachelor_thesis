ef5e724ca39c2586f58539c492f293aa678999bd
tangboshi <info@alexander-pastor.de>
2017-07-25 16:50:38 +0200

    automatic code push

--- measurements/debug/jobs_open/csma_2nodes_comprehensive_1.sh
+++ measurements/debug/jobs_open/csma_2nodes_comprehensive_1.sh
@@ -3,9 +3,11 @@ echo sifs = 1ms
 echo difs = 5ms
 echo backoff = 2ms
 echo nodes = 2
+echo timer = 300
+echo repetions = 5
 
-export timer=20
-export measurement_repetitions=2
+export timer=300
+export measurement_repetitions=5
 measurement_scripts=( csma_80211_IV.py csma_80211_V.py )
 plot_scripts=( rtt_2.py throughput.py )
 #################################################

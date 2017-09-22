4183ca0ce5ad206359a3860285ca4dcbf0691c0e
tangboshi <info@alexander-pastor.de>
2017-09-22 12:23:42 +0200

    automatic code push

--- measurements/py/evaluation.py
+++ measurements/py/evaluation.py
@@ -59,7 +59,7 @@ channel_occupation_mode     =   {
                                 }
 
 sniffer_settings            =   {
-                                    "link":     2,
+                                    "link":     1,
                                     "zoom":     [0.5,2.5]
                                 }
 

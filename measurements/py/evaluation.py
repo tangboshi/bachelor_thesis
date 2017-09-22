2d1e81841fca99884c93e014ffd5fb10ce92a76f
tangboshi <info@alexander-pastor.de>
2017-09-22 16:19:18 +0200

    automatic code push

--- measurements/py/evaluation.py
+++ measurements/py/evaluation.py
@@ -63,7 +63,7 @@ sniffer_settings            =   {
                                     "link":                     2,
                                     "zoom":                     [1.2,1.8],
                                     "smoothing_difference":     0.0001,
-                                    "smoothing_derivative":     1.1
+                                    "smoothing_derivative":     1.1,
                                     "smoothing_range":          [0.0010,0.0013]
                                 }
 

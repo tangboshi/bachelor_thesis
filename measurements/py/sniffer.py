80584e34b83cad93309912b5417bf046b8c33fd9
tangboshi <info@alexander-pastor.de>
2017-09-25 11:35:40 +0200

    automatic code push

--- measurements/py/sniffer.py
+++ measurements/py/sniffer.py
@@ -120,7 +120,7 @@ class sniffer:
                             tmp3.append(tmp)
                             tmp4.append(tmp2)
                         tmp, tmp2 = [], []
-                        if interval_upper_bound + zoom_interval <= self.sniffer_settings["zoom"][1]:
+                        if interval_upper_bound < self.sniffer_settings["zoom"][1]:
                             interval_lower_bound = interval_upper_bound
                             interval_upper_bound += zoom_interval
                     else:

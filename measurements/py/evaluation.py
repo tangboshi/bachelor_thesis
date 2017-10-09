6330534be9d170fbea6ef392985fe4c88296b384
tangboshi <info@alexander-pastor.de>
2017-10-09 18:06:06 +0200

    automatic code push

--- measurements/py/evaluation.py
+++ measurements/py/evaluation.py
@@ -45,7 +45,7 @@ custom_legend_coordinates   = {
                             }
 
 create_plots                = {
-                                "rtt":                  False,
+                                "rtt":                  True,
                                 "packet_loss":          False,
                                 "retxs":                False,
                                 "throughput":           True,
